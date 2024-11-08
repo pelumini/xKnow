0a8ea50cbbb2bb8c53875bb99c453e6823c207c0
pelumini <pelumini@gmail.com>
2024-11-08 05:32:00 +0000

    dashboard components added

---
+++ src/app/(dashboard)/_components/MobileSidebar.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Sidebar } from "./Sidebar";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <SheetHeader className="hidden">
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when youre done.
+          </SheetDescription>
+        </SheetHeader>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
